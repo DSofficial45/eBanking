db61b9ab2c5912f9666c852f174b5f401a091c7d
DESKTOP-BKD77DO\ADMIN <Dremyon@gmail.com>
2024-10-01 16:18:05 -0300

    .

--- BackEnd/Controller/sesionController.php
+++ BackEnd/Controller/sesionController.php
@@ -10,52 +10,20 @@
         case "registrar":
             registrarUsuario();
         break;
-    
-        case "cerrar":
-            cerrarSesion();
-            break;
-    
-        case "obtener":
-            obtenerSesion();
-            break;
-        
-        case "ver":
-            verUsuario();
-        break;
-    }
-
-    function obtenerSesion(){
-        $resultado = (new SesionDAO())->obtenerSesion();
-        echo json_encode($resultado);
-    }
-
-    function verUsuario(){
-        //$resultado = (new SesionDAO())->verUsuario();
-        //echo $resultado;
     }
     
     function registrarUsuario(){
         $email = $_POST['email'];
         $nombre = $_POST['nombre'];
-        $apellido = $_POST['apellido'];
-        $telefono = $_POST['telefono'];
         $password = $_POST['password'];
-        $isAdmin = $_POST['isAdmin'];
-        $resultado = (new SesionDAO())->registrarUsuario(email: $email, nombre: $nombre, apellido: $apellido, telefono: $telefono, password: $password, isAdmin: $isAdmin);
+        $resultado = (new SesionDAO())->registrarUsuario($email, $nombre, $password);
         echo json_encode($resultado);
     }
 
     function iniciarSesion(){
-        $nombre = $_POST['nombre'];
+        $email = $_POST['email'];
         $password = $_POST['password'];
-        $resultado = (new SesionDAO())->iniciarSesion($nombre, $password);
+        $resultado = (new SesionDAO())->registrarUsuario($email, $password);
         echo json_encode($resultado);
     }
-    
-    function cerrarSesion(){
-        $email = $_POST['id'];
-        $resultado = (new SesionDAO())->cerrarSesion($email);
-        echo json_encode($resultado);
-    }
-
 ?>
